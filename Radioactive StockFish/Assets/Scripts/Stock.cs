6ca85693646b39f79efc047165c902ec3d40552f
Morten Telling <mtelling1995@gmail.com>
2016-04-16 18:17:01 +0200

    Added Stock class
    added stock class

---
+++ Radioactive StockFish/Assets/Scripts/Stock.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stock : MonoBehaviour {
+
+	private Stack<double> rates;
+	private String name;
+	private String imgPath;
+	
+	public Stock (String name)
+	{
+		this.name = name;
+		this.rates = new Stack<double> ();
+	}
+	
+	public String GetImgPath() {
+		return this.imgPath;
+	}
+	
+	public void SetImgPath(String imgPath) {
+		this.imgPath = imgPath;
+	}
+	
+	public void SetNextRate(double rate) {
+		this.rates.push (rate);
+	}
+	
+	public Stack<double> getRates() {
+		return this.rates;
+	}
+}
