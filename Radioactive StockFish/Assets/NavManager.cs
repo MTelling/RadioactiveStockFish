202a779618473bb2a15352bd19f3e9ac791c7a27
Ivan Dimov <ivan0312@gmail.com>
2016-04-17 11:00:04 +0200

    Navigation Fix - Bet PopUp

--- Radioactive StockFish/Assets/NavManager.cs
+++ Radioactive StockFish/Assets/NavManager.cs
@@ -4,32 +4,36 @@ using UnityEngine.UI;
 
 public class NavManager : MonoBehaviour {
 
-    public Image popUp;
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public GameObject BetPopUP_object;
+    public GameObject BuyPanel_object;
+    public GameObject OverviewPanel_object;
+    // Use this for initialization
+    void Start () {
+
+       BetPopUP_object = GameObject.FindGameObjectWithTag("BetPopUP");
+       BuyPanel_object = GameObject.FindGameObjectWithTag("BuyPanel");
+       OverviewPanel_object = GameObject.FindGameObjectWithTag("OverviewPanel");
+}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (GameObject.FindGameObjectWithTag("BetPopUP") != null && GameObject.FindGameObjectWithTag("BetPopUP").active)
+            if (BetPopUP_object.active)
             {
-                GameObject.FindGameObjectWithTag("BetPopUP").active = false;
+                BetPopUP_object.active = false;
             }
-            else if (GameObject.FindGameObjectWithTag("BuyPanel") != null && GameObject.FindGameObjectWithTag("BuyPanel").active)
+            else if (BuyPanel_object.active)
             {
-                GameObject.FindGameObjectWithTag("BuyPanel").active = false;
-                GameObject.FindGameObjectWithTag("OverviewPanel").active = true;
+                BuyPanel_object.active = false;
+                OverviewPanel_object.active = true;
             }
         }	    
 	}
 
     public void OpenBetPopUP()
     {
-         if(GameObject.FindGameObjectWithTag("BetPopUP") != null)
-            GameObject.FindGameObjectWithTag("BetPopUP").SetActive(true);
+        BetPopUP_object.active = true;
     }
 
 }
