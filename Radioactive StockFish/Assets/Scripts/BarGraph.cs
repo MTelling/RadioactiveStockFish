f73155a10a712d20835f83b9466dcb6f1370e40f
Tobias Lindstrøm <Toli.TobiasLindstrom@gmail.com>
2016-04-17 05:20:22 +0200

    Fixed BarGraph
    Fixed the bargraph so it actually draws the graph, and made it more
    flexible

--- Radioactive StockFish/Assets/Scripts/BarGraph.cs
+++ Radioactive StockFish/Assets/Scripts/BarGraph.cs
@@ -6,30 +6,37 @@ using System;
 
 public class BarGraph : MonoBehaviour {
 
-    public int barSize = 15;
+    public int barCount = 15;
+    public int barSize = 48;
     public GameObject barPrefab;
 
     public void Start() {
-        for(int i = 0; i < barSize; i++) {
-            GameObject bar = Instantiate(barPrefab, new Vector3(48 + (48 * i), 48, 0), Quaternion.identity) as GameObject;
+        for(int i = 0; i < barCount; i++) {
+            GameObject bar = Instantiate(barPrefab, new Vector3(barSize + (barSize * i), barSize, 0), Quaternion.identity) as GameObject;
             bar.name = "Bar #" + i;
-            bar.transform.parent = this.transform;
+            bar.transform.SetParent(this.transform, true);
         }
+
+        Stock a = new Stock("Amazon", 100, 0.03);
+        for (int i = 0; i < barCount; i++) a.SetNextRate();
+        this.drawGraph(a);
     }
 
     public void drawGraph(Stock stock) {
         Stack<double> dupeStack = new Stack<double>(stock.GetRates());
         double[] priceList = dupeStack.ToArray();
 
-        if (priceList.Length >= barSize) {
+        if (priceList.Length >= barCount) {
             int maxSize = 0;
-            for (int i = priceList.Length - 1; i > priceList.Length - barSize; i--) {
-                if (priceList[i] > maxSize) maxSize = (int) (Math.Ceiling(priceList[i] / 100d) * 100);
+            for (int i = priceList.Length - 1; i > priceList.Length - barCount; i--) {
+                if (priceList[i] > maxSize) maxSize = (int) (Math.Ceiling(priceList[i] / 100d));
+                Debug.Log(Math.Ceiling((priceList[i] / 100d) * 100));
             }
 
-            for (int i = barSize - 1; i >= 0; i--) {
+            for (int i = barCount - 1; i >= 0; i--) {
                 int percentageValue = (int)Math.Floor(dupeStack.Pop() / maxSize);
                 this.transform.GetChild(i).GetComponent<Slider>().value = percentageValue;
+                Debug.Log(percentageValue);
             }
         }
     }
